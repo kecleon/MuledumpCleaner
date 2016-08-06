31fe9b27aa55b26d9c2ad86ce3a65fe71a058825
kecleon <ultimatedood@gmail.com>
2016-08-06 00:24:27 -0700

    Initial file commit
    updated

---
+++ MuledumpCleaner/MainForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MuledumpCleaner
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.outputTextbox = new System.Windows.Forms.TextBox();
+			this.loadAccountsButton = new System.Windows.Forms.Button();
+			this.fileLabel = new System.Windows.Forms.Label();
+			this.cleanButton = new System.Windows.Forms.Button();
+			this.progressLabel = new System.Windows.Forms.Label();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.outputBannedCheckbox = new System.Windows.Forms.CheckBox();
+			this.outputErrorAccountsCheckbox = new System.Windows.Forms.CheckBox();
+			this.saveButton = new System.Windows.Forms.Button();
+			this.progressProgressLabel = new System.Windows.Forms.Label();
+			this.fixAccountListFileCheckbox = new System.Windows.Forms.CheckBox();
+			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+			this.saveAsMuledumpFormatCheckbox = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// outputTextbox
+			// 
+			this.outputTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+			this.outputTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.outputTextbox.ForeColor = System.Drawing.Color.White;
+			this.outputTextbox.Location = new System.Drawing.Point(12, 108);
+			this.outputTextbox.Multiline = true;
+			this.outputTextbox.Name = "outputTextbox";
+			this.outputTextbox.Size = new System.Drawing.Size(582, 281);
+			this.outputTextbox.TabIndex = 0;
+			// 
+			// loadAccountsButton
+			// 
+			this.loadAccountsButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.loadAccountsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.loadAccountsButton.ForeColor = System.Drawing.Color.White;
+			this.loadAccountsButton.Location = new System.Drawing.Point(12, 12);
+			this.loadAccountsButton.Name = "loadAccountsButton";
+			this.loadAccountsButton.Size = new System.Drawing.Size(190, 30);
+			this.loadAccountsButton.TabIndex = 1;
+			this.loadAccountsButton.Text = "Load";
+			this.loadAccountsButton.UseVisualStyleBackColor = false;
+			this.loadAccountsButton.Click += new System.EventHandler(this.loadAccountsButton_Click);
+			// 
+			// fileLabel
+			// 
+			this.fileLabel.AutoSize = true;
+			this.fileLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+			this.fileLabel.ForeColor = System.Drawing.Color.White;
+			this.fileLabel.Location = new System.Drawing.Point(12, 50);
+			this.fileLabel.Name = "fileLabel";
+			this.fileLabel.Size = new System.Drawing.Size(61, 13);
+			this.fileLabel.TabIndex = 2;
+			this.fileLabel.Text = "accounts.js";
+			// 
+			// cleanButton
+			// 
+			this.cleanButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.cleanButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cleanButton.ForeColor = System.Drawing.Color.White;
+			this.cleanButton.Location = new System.Drawing.Point(208, 12);
+			this.cleanButton.Name = "cleanButton";
+			this.cleanButton.Size = new System.Drawing.Size(190, 30);
+			this.cleanButton.TabIndex = 3;
+			this.cleanButton.Text = "Clean";
+			this.cleanButton.UseVisualStyleBackColor = false;
+			this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
+			// 
+			// progressLabel
+			// 
+			this.progressLabel.AutoSize = true;
+			this.progressLabel.ForeColor = System.Drawing.Color.White;
+			this.progressLabel.Location = new System.Drawing.Point(78, 67);
+			this.progressLabel.Name = "progressLabel";
+			this.progressLabel.Size = new System.Drawing.Size(24, 13);
+			this.progressLabel.TabIndex = 4;
+			this.progressLabel.Text = "0/0";
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.FileName = "accounts.js";
+			// 
+			// outputBannedCheckbox
+			// 
+			this.outputBannedCheckbox.AutoSize = true;
+			this.outputBannedCheckbox.ForeColor = System.Drawing.Color.White;
+			this.outputBannedCheckbox.Location = new System.Drawing.Point(15, 85);
+			this.outputBannedCheckbox.Name = "outputBannedCheckbox";
+			this.outputBannedCheckbox.Size = new System.Drawing.Size(128, 17);
+			this.outputBannedCheckbox.TabIndex = 5;
+			this.outputBannedCheckbox.Text = "List banned accounts";
+			this.outputBannedCheckbox.UseVisualStyleBackColor = true;
+			// 
+			// outputErrorAccountsCheckbox
+			// 
+			this.outputErrorAccountsCheckbox.AutoSize = true;
+			this.outputErrorAccountsCheckbox.ForeColor = System.Drawing.Color.White;
+			this.outputErrorAccountsCheckbox.Location = new System.Drawing.Point(208, 85);
+			this.outputErrorAccountsCheckbox.Name = "outputErrorAccountsCheckbox";
+			this.outputErrorAccountsCheckbox.Size = new System.Drawing.Size(153, 17);
+			this.outputErrorAccountsCheckbox.TabIndex = 6;
+			this.outputErrorAccountsCheckbox.Text = "List all accounts with errors";
+			this.outputErrorAccountsCheckbox.UseVisualStyleBackColor = true;
+			// 
+			// saveButton
+			// 
+			this.saveButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.saveButton.ForeColor = System.Drawing.Color.White;
+			this.saveButton.Location = new System.Drawing.Point(404, 12);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(190, 30);
+			this.saveButton.TabIndex = 7;
+			this.saveButton.Text = "Save";
+			this.saveButton.UseVisualStyleBackColor = false;
+			this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+			// 
+			// progressProgressLabel
+			// 
+			this.progressProgressLabel.AutoSize = true;
+			this.progressProgressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.progressProgressLabel.ForeColor = System.Drawing.Color.White;
+			this.progressProgressLabel.Location = new System.Drawing.Point(12, 67);
+			this.progressProgressLabel.Name = "progressProgressLabel";
+			this.progressProgressLabel.Size = new System.Drawing.Size(60, 13);
+			this.progressProgressLabel.TabIndex = 8;
+			this.progressProgressLabel.Text = "Progress:";
+			// 
+			// fixAccountListFileCheckbox
+			// 
+			this.fixAccountListFileCheckbox.AutoSize = true;
+			this.fixAccountListFileCheckbox.Checked = true;
+			this.fixAccountListFileCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.fixAccountListFileCheckbox.ForeColor = System.Drawing.Color.White;
+			this.fixAccountListFileCheckbox.Location = new System.Drawing.Point(404, 85);
+			this.fixAccountListFileCheckbox.Name = "fixAccountListFileCheckbox";
+			this.fixAccountListFileCheckbox.Size = new System.Drawing.Size(189, 17);
+			this.fixAccountListFileCheckbox.TabIndex = 9;
+			this.fixAccountListFileCheckbox.Text = "Clean account list (disable if errors)";
+			this.fixAccountListFileCheckbox.UseVisualStyleBackColor = true;
+			// 
+			// saveAsMuledumpFormatCheckbox
+			// 
+			this.saveAsMuledumpFormatCheckbox.AutoSize = true;
+			this.saveAsMuledumpFormatCheckbox.ForeColor = System.Drawing.Color.White;
+			this.saveAsMuledumpFormatCheckbox.Location = new System.Drawing.Point(404, 66);
+			this.saveAsMuledumpFormatCheckbox.Name = "saveAsMuledumpFormatCheckbox";
+			this.saveAsMuledumpFormatCheckbox.Size = new System.Drawing.Size(145, 17);
+			this.saveAsMuledumpFormatCheckbox.TabIndex = 10;
+			this.saveAsMuledumpFormatCheckbox.Text = "Save in muledump format";
+			this.saveAsMuledumpFormatCheckbox.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+			this.ClientSize = new System.Drawing.Size(606, 401);
+			this.Controls.Add(this.saveAsMuledumpFormatCheckbox);
+			this.Controls.Add(this.fixAccountListFileCheckbox);
+			this.Controls.Add(this.progressProgressLabel);
+			this.Controls.Add(this.saveButton);
+			this.Controls.Add(this.outputErrorAccountsCheckbox);
+			this.Controls.Add(this.outputBannedCheckbox);
+			this.Controls.Add(this.progressLabel);
+			this.Controls.Add(this.cleanButton);
+			this.Controls.Add(this.fileLabel);
+			this.Controls.Add(this.loadAccountsButton);
+			this.Controls.Add(this.outputTextbox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Muledump Cleaner by 059 version 1.0";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox outputTextbox;
+		private System.Windows.Forms.Button loadAccountsButton;
+		private System.Windows.Forms.Label fileLabel;
+		private System.Windows.Forms.Button cleanButton;
+		private System.Windows.Forms.Label progressLabel;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+		private System.Windows.Forms.CheckBox outputBannedCheckbox;
+		private System.Windows.Forms.CheckBox outputErrorAccountsCheckbox;
+		private System.Windows.Forms.Button saveButton;
+		private System.Windows.Forms.Label progressProgressLabel;
+		private System.Windows.Forms.CheckBox fixAccountListFileCheckbox;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog;
+		private System.Windows.Forms.CheckBox saveAsMuledumpFormatCheckbox;
+	}
+}
+
---
+++ MuledumpCleaner/MainForm.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuledumpCleaner
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		Dictionary<string, string> FullList = new Dictionary<string, string>(); //full untampered list
+		Dictionary<string, string> InvalidList = new Dictionary<string, string>(); //Incorrect account details
+		Dictionary<string, string> BannedList = new Dictionary<string, string>(); //Account under maintenance
+		Dictionary<string, string> MigrateList = new Dictionary<string, string>(); //Account needs migration
+		Dictionary<string, string> ErrorList = new Dictionary<string, string>(); //<Error>, but not under maintenance
+		Dictionary<string, string> MiscList = new Dictionary<string, string>(); //neither <Chars> nor <Error>
+		Dictionary<string, string> CleanList = new Dictionary<string, string>(); //pruned list
+		
+		const string CHAR_LIST = "https://realmofthemadgod.appspot.com/char/list?guid=";
+
+		string file;
+
+		int accountCounter = 0;
+
+		string[] fullTextBuffer = new string[] { };
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			outputTextbox.AppendText("Initialized, click Load to select a file with a list of accounts, Clean to purge banned or invalid accounts, then Save to save the rest to a new file\n");
+			cleanButton.Enabled = false;
+			saveButton.Enabled = false;
+		}
+
+		private void updateControlText(Control control, string text, bool isTextBox = false)
+		{
+			if (isTextBox)
+			{
+				TextBox textbox = (TextBox)control;
+				textbox.BeginInvoke((MethodInvoker)delegate ()
+				{
+					textbox.AppendText(text);
+				});
+			}
+			else
+			{
+				Label label = (Label)control;
+				label.BeginInvoke((MethodInvoker)delegate ()
+				{
+					label.Text = text;
+				});
+			}
+		}
+
+		private void updateControlEnabled(Control control, bool enabled)
+		{
+			control.BeginInvoke((MethodInvoker)delegate ()
+			{
+				control.Enabled = enabled;
+			});
+		}
+
+		private void loadAccountsButton_Click(object sender, EventArgs e)
+		{
+			DialogResult result = openFileDialog.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				file = openFileDialog.FileName;
+				fileLabel.Text = file;
+				try
+				{
+					fullTextBuffer = File.ReadAllLines(file);
+				}
+				catch (IOException)
+				{
+					fileLabel.Text = "Error reading file";
+				}
+			}
+			else
+			{
+				fileLabel.Text = "Error opening file";
+			}
+			FullList.Clear();
+			InvalidList.Clear();
+			BannedList.Clear();
+			MigrateList.Clear();
+			ErrorList.Clear();
+			MiscList.Clear();
+			CleanList.Clear();
+			foreach (string line in fullTextBuffer)
+			{
+				string s = line;
+				if (s.Length >= 2)
+				{
+					if (fixAccountListFileCheckbox.Checked)
+					{
+						s = Regex.Replace(s, @"\s*'(.*)'\s*:\s*'(.*)'\s*,\s*", "$1:$2");
+						s = Regex.Replace(s, @"\+", "%2B");
+
+						if (s.EndsWith(","))
+						{
+							s = s.Substring(0, s.Length - 1);
+						}
+					}
+					string[] acc = s.Split(':');
+					if (acc.Length == 2)
+					{
+						if (FullList.ContainsKey(acc[0]))
+						{
+							if (acc[1] != FullList[acc[0]])
+							{
+								DialogResult dr = MessageBox.Show("Duplicate account found\n" + "\nReplace the original:\n" + acc[0] + " : " + FullList[acc[0]] + "\nwith:\n" + acc[0] + " : " + acc[1] + "\n?", "Duplicate Account", MessageBoxButtons.YesNo);
+								if (dr == DialogResult.Yes)
+								{
+									FullList[acc[0]] = acc[1];
+								}
+							}
+						}
+						else
+						{
+							FullList.Add(acc[0], acc[1]);
+						}
+					}
+					/*else
+					{
+						if (false)
+						{
+							outputTextbox.AppendText("String did not contain email:password (" + s + ")\n");
+						}
+					}*/
+				}
+			}
+			outputTextbox.AppendText("Read all accounts\n");
+			cleanButton.Enabled = true;
+		}
+
+		private void cleanButton_Click(object sender, EventArgs e)
+		{
+			InvalidList.Clear();
+			BannedList.Clear();
+			MigrateList.Clear();
+			ErrorList.Clear();
+			MiscList.Clear();
+			CleanList.Clear();
+			Task.Run(() => { processNextAccount(); });
+			cleanButton.Enabled = false;
+		}
+
+		private void processNextAccount()
+		{
+			updateControlText(progressLabel, accountCounter + "/" + FullList.Count);
+			//progressLabel.Update();
+			KeyValuePair<string, string> acc = new KeyValuePair<string, string>();
+			try
+			{
+				acc = FullList.ElementAt(accountCounter);
+			}
+			catch
+			{
+				updateControlText(outputTextbox, "Finished\n", true);
+				updateControlEnabled(cleanButton, true);
+				updateControlEnabled(saveButton, true);
+				accountCounter = 0;
+				return;
+			}
+			WebClient wc = new WebClient();
+			wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(wc_DownloadStringCompleted);
+			wc.DownloadStringAsync(new Uri(CHAR_LIST + acc.Key + "&password=" + acc.Value));
+		}
+
+		void wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+		{
+			string text = e.Result;
+			var acc = FullList.ElementAt(accountCounter);
+			if (text.StartsWith("<Error>"))
+			{
+				if (text.Contains("Account is under maintenance"))
+				{
+					updateControlText(outputTextbox, "Added banned account: " + acc.Key + ':' + acc.Value + '\n', true);
+					BannedList.Add(acc.Key, acc.Value);
+				}
+				else if (text.Contains("Account credentials not valid"))
+				{
+					updateControlText(outputTextbox, "Added invalid credential account: " + acc.Key + ':' + acc.Value + '\n', true);
+					InvalidList.Add(acc.Key, acc.Value);
+				}
+				else
+				{
+					updateControlText(outputTextbox, "Added unknown account error account: " + acc.Key + ':' + acc.Value + '\n', true);
+					ErrorList.Add(acc.Key, acc.Value);
+				}
+			}
+			else if (text.StartsWith("<Chars"))
+			{
+				updateControlText(outputTextbox, "Added clean account: " + acc.Key + ':' + acc.Value + '\n', true);
+				CleanList.Add(acc.Key, acc.Value);
+			}
+			else if (text.StartsWith("<Migrate>"))
+			{
+				updateControlText(outputTextbox, "Added migrate account: " + acc.Key + ':' + acc.Value + '\n', true);
+				MigrateList.Add(acc.Key, acc.Value);
+			}
+			else
+			{
+				updateControlText(outputTextbox, "Added unknown error account: " + acc.Key + ':' + acc.Value + '\n', true);
+				MiscList.Add(acc.Key, acc.Value);
+			}
+			accountCounter++;
+			processNextAccount();
+		}
+
+		private void saveButton_Click(object sender, EventArgs e)
+		{
+			StringBuilder finalList = new StringBuilder();
+			if (saveAsMuledumpFormatCheckbox.Checked)
+			{
+				finalList.Append("accounts = {\r\n");
+			}
+			if (CleanList.Count > 0)
+			{
+				finalList.Append("//Clean, unbanned accounts\r\n");
+			}
+			foreach (var acc in CleanList)
+			{
+				if (saveAsMuledumpFormatCheckbox.Checked)
+				{
+					finalList.Append("\'");
+				}
+				finalList.Append(acc.Key);
+				if (saveAsMuledumpFormatCheckbox.Checked)
+				{
+					finalList.Append("\':\'");
+				}
+				else
+				{
+					finalList.Append(':');
+				}
+				finalList.Append(acc.Value);
+				if (saveAsMuledumpFormatCheckbox.Checked)
+				{
+					finalList.Append("\',");
+				}
+				finalList.Append("\r\n");
+			}
+			if (outputBannedCheckbox.Checked)
+			{
+				if (BannedList.Count > 0)
+				{
+					finalList.Append("\n\n//Banned accounts\r\n");
+				}
+				foreach (var acc in BannedList)
+				{
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\'");
+					}
+					finalList.Append(acc.Key);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\':\'");
+					}
+					else
+					{
+						finalList.Append(':');
+					}
+					finalList.Append(acc.Value);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\',");
+					}
+					finalList.Append("\r\n");
+				}
+			}
+			if (outputErrorAccountsCheckbox.Checked)
+			{
+				if (ErrorList.Count > 0)
+				{
+					finalList.Append("\n\n//Errors - char/list result starts with <Error> but account is not banned\r\n");
+				}
+				foreach (var acc in ErrorList)
+				{
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\'");
+					}
+					finalList.Append(acc.Key);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\':\'");
+					}
+					else
+					{
+						finalList.Append(':');
+					}
+					finalList.Append(acc.Value);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\',");
+					}
+					finalList.Append("\r\n");
+				}
+				if (InvalidList.Count > 0)
+				{
+					finalList.Append("\n\n//Invalid credential\r\n");
+				}
+				foreach (var acc in InvalidList)
+				{
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\'");
+					}
+					finalList.Append(acc.Key);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\':\'");
+					}
+					else
+					{
+						finalList.Append(':');
+					}
+					finalList.Append(acc.Value);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\',");
+					}
+					finalList.Append("\r\n");
+				}
+
+				if (MigrateList.Count > 0)
+				{
+					finalList.Append("\n\n//Accounts that need(ed) migration\r\n");
+				}
+				foreach (var acc in MigrateList)
+				{
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\'");
+					}
+					finalList.Append(acc.Key);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\':\'");
+					}
+					else
+					{
+						finalList.Append(':');
+					}
+					finalList.Append(acc.Value);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\',");
+					}
+					finalList.Append("\r\n");
+				}
+				if (MiscList.Count > 0)
+				{
+					finalList.Append("\n\n//Miscelaneous parse errors - char/list result does not start with <Error> or <Chars\r\n");
+				}
+				foreach (var acc in MiscList)
+				{
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\'");
+					}
+					finalList.Append(acc.Key);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\':\'");
+					}
+					else
+					{
+						finalList.Append(':');
+					}
+					finalList.Append(acc.Value);
+					if (saveAsMuledumpFormatCheckbox.Checked)
+					{
+						finalList.Append("\',");
+					}
+					finalList.Append("\r\n");
+				}
+			}
+			if (saveAsMuledumpFormatCheckbox.Checked)
+			{
+				finalList.Append("}\r\n");
+			}
+			string finalString = finalList.ToString();
+			saveFileDialog.InitialDirectory = Path.GetDirectoryName(file);
+			if(saveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				outputTextbox.AppendText("File saved as:" + saveFileDialog.FileName + '\n');
+			}
+			File.WriteAllText(saveFileDialog.FileName, finalString);
+		}
+	}
+}
